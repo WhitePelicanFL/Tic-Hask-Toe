fe83d9c6b6549e4f9442a6bd789fe100d68981b2
Eddie Sutton <eddie.sutton.stpete@gmail.com>
2023-01-24 15:48:27 +0000

    Commit for Jan 24 10:45

--- solutions/A1.hs
+++ solutions/A1.hs
@@ -5,65 +5,62 @@ import Data.Char (toUpper)
 -- *** Assignment 1-1 *** --
 
 -- Q#01
-
 _SIZE_ = 3
 
 -- Q#02
-
 _DISPLAY_LOGO_ :: Bool
 _DISPLAY_LOGO_ = True
 d = _DISPLAY_LOGO_
 
-
 -- Q#03
-
 convertRowIndex :: Char -> Int
 convertRowIndex char = fromEnum (toUpper (char)) - 65
 
-
 -- Q#04
-
 _INVALID_MOVE_ :: (Int, Int)
 _INVALID_MOVE_ = (-1, -1)
 
 -- Q#05
-
 _SEP_ :: [Char]
 _SEP_ = ['_','|','_']
+_SEP2_ :: String
+_SEP2_ = "_|_"
+
 
 -- *** Assignment 1-2 *** --
 
 -- Q#06
-data Square
+data Square = X | O | Neither deriving (Show, Eq)
 
+-- not so sure about this one
+type Move   = (Int, Int)
 
 -- Q#07
-data GameState
-
+data GameState = GameIsInProgress | GameIsTied | OWonTheGame | XWonTheGame deriving (Show, Eq)
 
 -- Q#08
-
-
-
-
-
+-- Some Synonyms
+type Player = Square
+type Row    = Square
+type Line   = Square
+type Board  = Row
 
 -- Q#09
+getFirstPlayerIf :: Bool -> Player
+getFirstPlayerIf firstPlayerIf =
+  if firstPlayerIf == True then X else O
 
-getFirstPlayer = undefined
-
-
-getFirstPlayer_ = undefined
+getFirstPlayerGd :: Bool -> Player
+getFirstPlayerGd firstPlayerGd
+  | firstPlayerGd == True  = X
+  | firstPlayerGd == False = O
+  | otherwise              = Neither
 
 -- Q#10
-
 showGameState gs = undefined
 
 -- Q#11
-
 switchPlayer = undefined
 
-
 -- Q#12
-
 showSquare = undefined
